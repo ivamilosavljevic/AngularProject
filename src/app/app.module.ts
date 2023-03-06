11c01c014a6b95ba367f1048b0844078677167e3
Iva Milosavljevic <ivamilosavljevic@live.com>
2023-03-06 14:46:51 +0100

    main components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
