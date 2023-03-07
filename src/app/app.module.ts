a856a259771f55ad8cbdb44b3b152ff5890c6fd4
Iva Milosavljevic <ivamilosavljevic@live.com>
2023-03-07 14:13:30 +0100

    edited tabs component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import { TabsComponent } from './tabs/tabs.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    TabsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTabsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
