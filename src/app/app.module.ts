4fdca9f573c2bb5cb3fed26c83c2942f294e70a1
frosinacvetkovska <frosinacvetkovska29@gmail.com>
2023-03-07 14:11:22 +0100

    header first edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
