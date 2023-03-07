71941aef871c6e359480f2a87478ed5eaa9d4aad
Iva Milosavljevic <ivamilosavljevic@live.com>
2023-03-07 17:00:12 +0100

    routing,pnf, events added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TabsComponent } from './tabs/tabs.component';
+import { EventsComponent } from './events/events.component';
+import { FooterComponent } from './footer/footer.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot([
+      {path:'', component:TabsComponent},
+      {path:'home', component:TabsComponent},
+      {path:'events', component:EventsComponent},
+      {path:'**', component:PageNotFoundComponent},
+
+
+    ])
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 import { HeaderComponent } from './header/header.component';
 
 import {MatTabsModule} from '@angular/material/tabs';
 import { TabsComponent } from './tabs/tabs.component';
+import { EventsComponent } from './events/events.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 
 
 
@@ -17,15 +21,18 @@ import { TabsComponent } from './tabs/tabs.component';
   declarations: [
     AppComponent,
     FooterComponent,
-    NavigationBarComponent,
     HeaderComponent,
-    TabsComponent
+    TabsComponent,
+    EventsComponent,
+    PageNotFoundComponent
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatTabsModule,
+    BrowserAnimationsModule,
+    MatCardModule
 
   ],
   providers: [],
